ad4c684f9abe9dd22334ac835b10a6000d4bc85f
Ujjwal Singh <usingh07860@gmail.com>
2024-09-03 16:38:05 +0530

    edit login & some responsiveness

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -71,33 +71,3 @@ export default function Login() {
     </div>
   )
 }
-
-
-
-{/* <div className="text-white h-[100vh] flex justify-center items-center bg-cover" style={{'background':'url()'}}>
-  login
-</div> */}
-
-{/* <div>
-  <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
-    <div>
-      <input type="text" className="block w-72 py-2.5 px-0 text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600" />
-      <label htmlFor="" className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"></label>
-    </div>
-    <div>
-      <input type="text" className="block w-72 py-2.5 px-0 text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600"/>
-      <label htmlFor="" className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"></label>
-    </div>
-    <div className="flex justify-center items-center">
-      <div className="flex gap-2 items-center">
-        <input type="checkbox" />
-        <label htmlFor="Remember Me"></label>
-      </div>
-      <span className="text-blue-500">Forgot Password?</span> // link
-    </div>
-    <button className="w-full md-4 text-[10px] mt-6 rounded-full bg-white text-emerald-600 hover:bg-emerald-600 hover:text-white py-2 transition-colors duration-300">Login</button>
-    <div>
-      <span className="m-4">New Here? <Link className="text-blue-500" to='/Register'></Link></span>
-    </div>
-  </div>
-</div> */}
